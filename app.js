1ae7a43a118960f83f8f4a8cb2182c90e2c681e3
Alexander Martinez <59891955+A-man624@users.noreply.github.com>
2025-03-15 18:10:47 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// Student Name: Alexander Martinez
+// Student ID: 1214728190
+// Date: 03/15/25
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Alexander Martinez IFT554 Module5");
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log("The server is running on port 3000");
+});
